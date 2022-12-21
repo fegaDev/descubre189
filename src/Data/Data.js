d3df153a3febf242c932928fbd22e824cc235055
fegaDev <fegadeveloper@gmail.com>
2022-12-21 15:21:31 -0500

    update data

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -36,7 +36,98 @@ export const Data = [
   }, 
   {
     id: 7,
-    frase: "La vida de primera clase empieza donde termina tu zona de confort",
-    autor: "Anónimo",
+    frase: "No importa cuánto tiempo tengas que esperar por tus sueños. Siempre valdrá la pena",
+    autor: "Paulo Coelho",
   },
+    {
+    id: 8,
+    frase: "El poder de una idea no puede ser medido mientras no se traduzca en acciones",
+    autor: "Napoleon Bonaparte",
+  },
+    {
+    id: 9,
+    frase: "Nunca dejes que lo imposible te detenga de intentar lo posible",
+    autor: "Mary McLeod Bethune",
+  },
+    {
+    id: 10,
+    frase: "No te rindas nunca, porque nunca sabes si el próximo intento será el que funcione",
+    autor: "Chris Gardner",
+  },
+  {
+   id: 11,
+    frase: "El éxito es la capacidad de ir de fracaso en fracaso sin perder el entusiasmo",
+    autor: "Winston Churchill",
+  },
+    {
+   id: 12,
+    frase: "Cada minuto que pasa es otra oportunidad para cambiarlo todo",
+    autor: "Vanilla Sky",
+  },
+    {
+   id: 13,
+    frase: "Vale más actuar exponiéndose a arrepentirse de ello, que arrepentirse de no haber hecho nada",
+    autor: "Giovanni Boccaccio",
+  },
+    {
+   id: 14,
+    frase: "Todos tenemos un destino. El nuestro es el que elegimos.",
+    autor: "Paulo Coelho",
+  },
+  {
+    id: 15,
+     frase: "La pereza viaja tan despacio que la pobreza no tarda en alcanzarla",
+     autor: "Benjamin Franklin",
+   },
+   {
+    id: 16,
+     frase: "No dejes que tus sueños se conviertan en simples ilusiones.",
+     autor: "Anónimo",
+   },
+   {
+    id: 17,
+     frase: "Todos tenemos un destino. El nuestro es el que elegimos.",
+     autor: "Paulo Coelho",
+   },
+   {
+    id: 18,
+     frase: "Nunca dejes que nadie te diga que no puedes hacer algo, ni siquiera yo. Si tienes un sueño, tienes que protegerlo. Las personas que no son capaces de hacer algo te dirán que tú tampoco puedes. Si quieres algo, ve a por ello",
+     autor: "Will Smith",
+   },
+   {
+    id: 19,
+    frase: "El éxito no es otra cosa que la aplicación diaria de la disciplina. Aléjate del 97% de la gente. No uses su vocabulario, no uses sus excusas, y únete al 3% restante",
+    autor: "Jim Rohn"
+   },
+   {
+    id: 20,
+    frase: "La vida es demasiado valiosa como para gastarla tratando de no ser tu misma.",
+    autor: "Anónimo"
+   },
+   {
+    id: 21,
+    frase: "Los sueños no cumplidos no mueren, simplemente se quedan dormidos esperando a ser despertados.",
+    autor: "Anónimo"
+   },
+   {
+    id: 22,
+    frase: "Si quieres algo que nunca has tenido, tienes que hacer algo que nunca has hecho.",
+    autor: "Thomas Jefferson"
+   },
+   {
+    id: 23,
+    frase: "Incluso la gente que afirma que no podemos hacer nada para cambiar nuestro destino, mira antes de cruzar la calle",
+    autor: "Stephen Hawking"
+   },
+   {
+    id: 24,
+    frase: "",
+    autor: ""
+   },
+   {
+    id: 25,
+    frase: "",
+    autor: ""
+   },
+   
 ];
